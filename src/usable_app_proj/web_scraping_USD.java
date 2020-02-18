a547d5530e63c35f34474891af66bcea3342506e
Emils Baskers <emils.baskers@gmail.com>
2020-02-18 21:35:47 +0200

    update

--- src/usable_app_proj/web_scraping_USD.java
+++ src/usable_app_proj/web_scraping_USD.java
@@ -1,5 +1,26 @@
 package usable_app_proj;
 
-public class web_scraping_USD {
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class web_scraping_USD {
+	
+	public web_scraping_USD() {
+	Document doc = null;
+	try{
+		doc = Jsoup.connect("https://www.bank.lv/statistika/dati-statistika/valutu-kursi/aktualie").userAgent("Mozilla/72.0.2").get();
+	}
+	
+	catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+	Elements curVal=doc.select("td.value");
+	double USD=Double.parseDouble(curVal.get(510).getElementsByTag("td").first().text());
+	}
+	
 }
